a5cc3de23dcdc16c112f5f9afa492ac716b198a8
saketpani <saketpani@gmail.com>
2023-05-08 12:20:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,78 +22,10 @@ def get_abs_url(url):
 
 
 @app.route('/')
-def home():
-    response = [{
-        "title" : "Ad Title",
-        "description" : "Ad Description"
-    }]
-    response2 = [{
-        "title" : "Ad Title",
-        "description" : "Ad Description"
-    }]
-
-    ads = response.json()
-    posts = response2.json()
-    return render_template("index.html", ads=ads, posts=posts)
+def home():   
+    return render_template("index.html")
 
 
-@app.route('/ad/add', methods=['GET'])
-def add_ad_view():
-    return render_template("new_ad.html")
-
-
-@app.route('/ad/edit/<id>', methods=['GET'])
-def edit_ad_view(id):
-    response = requests.get(settings.API_URL + '/getAdvertisement?id=' + id)
-    ad = response.json()
-    return render_template("edit_ad.html", ad=ad)
-
-
-@app.route('/ad/delete/<id>', methods=['GET'])
-def delete_ad_view(id):
-    response = requests.get(settings.API_URL + '/getAdvertisement?id=' + id)
-    ad = response.json()
-    return render_template("delete_ad.html", ad=ad)
-
-@app.route('/ad/view/<id>', methods=['GET'])
-def view_ad_view(id):
-    response = requests.get(settings.API_URL + '/getAdvertisement?id=' + id)
-    ad = response.json()
-    return render_template("view_ad.html", ad=ad)
-
-@app.route('/ad/new', methods=['POST'])
-def add_ad_request():
-    # Get item from the POST body
-    req_data = {
-        'title': request.form['title'],
-        'city': request.form['city'],
-        'description': request.form['description'],
-        'email': request.form['email'],
-        'imgUrl': request.form['imgUrl'],
-        'price': request.form['price']
-    }
-    response = requests.post(settings.API_URL + '/createAdvertisement', json=json.dumps(req_data))
-    return redirect(url_for('home'))
-
-@app.route('/ad/update/<id>', methods=['POST'])
-def update_ad_request(id):
-    # Get item from the POST body
-    req_data = {
-        'title': request.form['title'],
-        'city': request.form['city'],
-        'description': request.form['description'],
-        'email': request.form['email'],
-        'imgUrl': request.form['imgUrl'],
-        'price': request.form['price']
-    }
-    response = requests.put(settings.API_URL + '/updateAdvertisement?id=' + id, json=json.dumps(req_data))
-    return redirect(url_for('home'))
-
-@app.route('/ad/delete/<id>', methods=['POST'])
-def delete_ad_request(id):
-    response = requests.delete(settings.API_URL + '/deleteAdvertisement?id=' + id)
-    if response.status_code == 200:
-        return redirect(url_for('home'))
 
 # running app
 def main():
